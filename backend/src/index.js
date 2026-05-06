cce4194f463ebfd1c4c8f2a19d714269b179750e
Novip1906 <novipcs@gmail.com>
2026-05-06 20:31:26 +0300

    [backend]: FEAT | add cors middleware for cross-origin frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,16 @@ const dataFilePath = path.join(__dirname, 'data', 'products.json');
 app.use(express.json());
 
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+    next();
+});
+
+app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
 });
